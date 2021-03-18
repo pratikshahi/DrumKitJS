343c8d763add350993dcf8aa715102218c1033a9
Pratik Shahi <shahipr0@gmail.com>
2021-03-18 11:07:47 +0530

    added BGimage through css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 for(var i=0;i<document.querySelectorAll(".drum").length;i++)
-document.querySelectorAll("button")[i].addEventListener("click",handleClick)
+document.querySelectorAll("button")[i].addEventListener("click",handleClick);
 
 function handleClick(){
-    alert("i got clicked");
+    var audio=new Audio("sounds/tom-1.mp3");
+    audio.play();
 }
